c17f7ad4fb07251f7954776f879cc90632ce7a28
HuaTsai <huatsai.eed07g@nctu.edu.tw>
2025-02-12 02:00:34 +0800

    update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>ph", "<CMD>Gitsigns preview_hunk<CR>", { desc = "gitsigns preview hunk" })
+map("n", "<leader>rh", "<CMD>Gitsigns reset_hunk<CR>", { desc = "gitsigns reset hunk" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,13 +13,52 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "bash",
+        "cpp",
+        "python",
+      },
+    },
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+    opts = function()
+      conf = require "nvchad.configs.nvimtree"
+      conf.filters.git_ignored = false
+      return conf
+    end,
+  },
+
+  {
+    "Pocco81/auto-save.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("auto-save").setup {}
+    end,
+  },
+
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()
+      vim.keymap.set("i", "<M-n>", "<Plug>(copilot-next)", {})
+      vim.keymap.set("i", "<M-p>", "<Plug>(copilot-previous)", {})
+      vim.keymap.set("i", "<M-l>", 'copilot#Accept("\\<CR>")', {
+        silent = true,
+        expr = true,
+        replace_keycodes = false,
+      })
+      vim.g.copilot_no_tab_map = true
+    end,
+  },
 }
