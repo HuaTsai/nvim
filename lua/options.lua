5ae4bd355e04687f674930fa610e80bfc91a48cf
HuaTsai <huatsai.eed07g@nctu.edu.tw>
2025-02-18 23:56:52 +0800

    c++ file comment

--- lua/options.lua
+++ lua/options.lua
@@ -2,6 +2,11 @@ require "nvchad.options"
 
 -- add yours here!
 
-local o = vim.o
-o.rnu = true
+vim.o.rnu = true
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cpp",
+  callback = function()
+    vim.bo.commentstring = "// %s"
+  end,
+})
 -- o.cursorlineopt ='both' -- to enable cursorline!
