be8f8feab1221a4a09072d77732d81e9c69a944a
HuaTsai <huatsai.eed07g@nctu.edu.tw>
2025-03-20 15:34:37 +0800

    markdown support

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "lua_ls", "clangd", "cmake", "pyright", "ruff", "jsonls" }
+local servers = { "html", "cssls", "lua_ls", "clangd", "cmake", "pyright", "ruff", "jsonls", "marksman" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
